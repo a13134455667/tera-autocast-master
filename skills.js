b759f796d7c10c9de211f91c09159c15a10ed578
Weisisi <54429834+a13134455667@users.noreply.github.com>
2021-12-15 18:26:08 +0800

    仓库初始化

---
+++ skills.js
@@ -0,0 +1,16 @@
+module.exports = [
+{ job: 4, group: 20, enabled: true, max_targets: 3, max_distance: 25}, //魔道 - 追蹤業火
+{ job: 4, group: 21, enabled: true, max_targets: 3, max_distance: 25}, //魔道 - 神經衰弱
+{ job: 4, group: 22, enabled: true, max_targets: 2, max_distance: 25}, //魔道 - 燃燒氣息
+{ job: 4, group: 23, enabled: true, max_targets: 1, max_distance: 25}, //魔道 - 精神耗竭
+{ job: 4, group: 25, enabled: true, max_targets: 2, max_distance: 25}, //魔道 - 束縛詛咒
+{ job: 6, group: 30, enabled: true, max_targets: 3, max_distance: 25}, //祭祀 - 乏力預言
+{ job: 6, group: 33, enabled: true, max_targets: 1, max_distance: 25}, //祭祀 - 伊莎拉搖籃曲
+{ job: 6, group: 35, enabled: true, max_targets: 1, max_distance: 25}, //祭祀 - 神聖閃電
+{ job: 7, group: 35, enabled: true, max_targets: 2, max_distance: 25}, //元素 - Volley of Curses
+{ job: 7, group: 35, enabled: true, max_targets: 2, max_distance: 25}, //元素 -  睡
+{ job: 7, group: 35, enabled: true, max_targets: 3, max_distance: 25}, //元素 -  Curse of Exhaustion
+{ job: 7, group: 35, enabled: true, max_targets: 3, max_distance: 25}, //元素 -  Curse of confusion
+{ job: 7, group: 35, enabled: true, max_targets: 1, max_distance: 25}, //元素 -  Mire
+{ job: 7, group: 35, enabled: true, max_targets: 1, max_distance: 25} //元素 -  Contagion
+]
